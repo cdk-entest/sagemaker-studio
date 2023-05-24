0a1991edc4af8b03787e63a5742c7067b7a12ce4
entest <hai@entst.io>
2023-05-24 11:13:39 +0000

    test

--- test/life-cycle-configuration/install-package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script installs a single pip package on a SageMaker Studio Kernel Application
-
-set -eux
-
-# PARAMETERS
-PACKAGE=pyarrow
-
-pip install --upgrade $PACKAGE
--- test/test.sh
+++
@@ -1,3 +0,0 @@
-aws sagemaker update-domain \
---domain-id d-rmxdg2gitvsb \
---domain-settings-for-update "ExecutionRoleIdentityConfig=USER_PROFILE_NAME"
